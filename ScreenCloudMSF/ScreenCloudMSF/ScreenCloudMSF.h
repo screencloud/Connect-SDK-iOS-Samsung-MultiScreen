b798addc28e65f6fbaaf8da6d8619070828ab867
Jirasak Saebang <cosmodesktop@gmail.com>
2015-07-27 14:48:50 +0700

    structuring the framework

---
+++ ScreenCloudMSF/ScreenCloudMSF/ScreenCloudMSF.h
@@ -0,0 +1,13 @@
+//
+//  ScreenCloudMSF.h
+//  ScreenCloudMSF
+//
+//  Created by Jirasak Saebang on 7/27/15.
+//  Copyright (c) 2015 codegent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScreenCloudMSF : NSObject
+
+@end
