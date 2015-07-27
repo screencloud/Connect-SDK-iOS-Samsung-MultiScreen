9c7b4530e19a36a4dcd24eacfe1a3da89a66b23d
Jirasak Saebang <cosmodesktop@gmail.com>
2015-07-27 16:12:23 +0700

    added MSF 1 and 2 to framework, added connectsdk to pod

---
+++ ScreenCloudMSFFramework/ScreenCloudMSFFramework/ScreenCloudMSFFramework.h
@@ -0,0 +1,22 @@
+//
+//  ScreenCloudMSFFramework.h
+//  ScreenCloudMSFFramework
+//
+//  Created by Jirasak Saebang on 7/27/15.
+//  Copyright (c) 2015 codegent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MultiScreenSErvice.m"
+
+@import MSF;
+
+//! Project version number for ScreenCloudMSFFramework.
+FOUNDATION_EXPORT double ScreenCloudMSFFrameworkVersionNumber;
+
+//! Project version string for ScreenCloudMSFFramework.
+FOUNDATION_EXPORT const unsigned char ScreenCloudMSFFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScreenCloudMSFFramework/PublicHeader.h>
+
+
---
+++ ScreenCloudMSFFramework/ScreenCloudMSFFramework/src/MultiScreenService.h
@@ -0,0 +1,22 @@
+//
+// Created by Jeremy White on 6/16/14.
+// Copyright (c) 2014 LG Electronics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#import <SamsungMultiscreen/SamsungMultiscreen.h>
+@import MSF;
+#import "DeviceService.h"
+#import "WebAppLauncher.h"
+
+#define kConnectSDKMultiScreenTVServiceId @"Samsung MultiScreen"
+
+
+@interface MultiScreenService : DeviceService <MediaPlayer, WebAppLauncher>
+
+/*!
+ * Read-only reference to native Samsung MultiScreen device object.
+ */
+@property (nonatomic, readonly) Service *device; // MSDevice *device;
+
+@end
